2d99cadf5b8514c83b00d7438778a7ec13c6fa37
Yoshiboi18303 <yoshiboi18303.t@gmail.com>
2022-11-28 00:09:44 -0500

    Update Batch Script

--- installLavalink.cmd
+++ installLavalink.cmd
@@ -2,6 +2,8 @@
 
 cls
 
-echo Installing Lavalink, this may take a few minutes depending on your internet connection... DO NOT EXIT OR DISCONNECT FROM THE INTERNET DURING THIS PROCESS.
+echo Installing Lavalink, this may take a few minutes depending on your internet connection...
+
+echo DO NOT EXIT OR DISCONNECT FROM THE INTERNET DURING THIS PROCESS.
 
 powershell -Command "Invoke-WebRequest https://github.com/freyacodes/Lavalink/releases/download/3.6.2/Lavalink.jar -OutFile Lavalink.jar"
